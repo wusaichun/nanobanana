d7c1772f31ada901d01907c719ca8b2e3de501ec
wusaichun <saichunwu853@gmail.com>
2025-12-24 13:52:24 +0800

    feat: Add new dashboard tool page, integrating `uuid` and `dotenv` for Drizzle configuration.

---
+++ app/[locale]/dashboard/my-tool/page.tsx
@@ -0,0 +1,8 @@
+export default function MyToolPage() {
+    return (
+        <div className="p-10 bg-blue-100 text-blue-800 rounded-xl">
+            <h1 className="text-2xl font-bold">这是我的第一个组件</h1>
+            <p>它已经集成在模版里了！</p>
+        </div>
+    )
+}
